a357ebacde42b1fcee01863b2f90e7b3d1d1085c
Student <example@example.com>
2018-11-13 17:28:24 -0800

    add ms test template  with correct files for testing

---
+++ numbergame.solution/numbergame.tests/ModelTests/GameTest.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using numbergame;
+
+namespace numbergame.Tests
+{
+  [TestClass]
+  public class GameTest
+  {
+    [TestMethod]
+    public void RunGuess_NumberBetweenOneAndOneHundred_RandomNumber()
+  {
+    // Game testGame = new Game();
+    // Assert.AreEqual(true,testGame.IsInitialValue(0));
+  }
+  }
+// } EXAMPLE BELOW
+// [TestMethod]
+// public void IsLeapYear_NumberDivisibleByFour_True()
+// {
+//   LeapYear testLeapYear = new LeapYear();
+//   Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+// }
+//
+// [TestMethod]
+// public void IsLeapYear_NumberNotDivisibleByFour_False()
+// {
+//   LeapYear testLeapYear = new LeapYear();
+//   Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+// }
